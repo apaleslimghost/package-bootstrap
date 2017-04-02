8527b0ddb7dd41c3c658ddda305b8c01fd9cf583
Matt Brennan <matt@153.io>
2017-04-02 10:21:14 +0100

    init

---
+++ index.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+echo $0
+
+files=$(env | grep npm_package_config_bootstrapFiles | cut -f 2 -d '=')
+destination=${1:-$(pwd)}
+
+copy_files() {
+	local IFS=$'\n'
+	for file in $files; do
+		if [ ! -e "$destination/$file" ]; then
+			cp "$file" "$destination/$file"
+			echo "  │ $file installed"
+		else
+			echo "  │ $file exists, skipping"
+		fi
+	done
+}
+
+if is-installing-package ; then
+	echo "  ⎘ installing bootstrap files for $npm_package_name to $destination"
+	copy_files
+	echo "  ✔︎ done"
+	echo
+fi
